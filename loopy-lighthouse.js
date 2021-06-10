2387ba49a7251a2b6c06c0c3b28e34b2c1762815
yhupaul <yhupaul@hotmail.com>
2021-06-09 18:27:50 -0700

    looy example

---
+++ loopy-lighthouse.js
@@ -0,0 +1,21 @@
+/*for (let x = 100; x <= 200; x++) {
+  if (x % 3 === 0 && x % 4 === 0) {
+    console.log("LoopyLighthouse");
+  } else if (x % 3 === 0) {
+    console.log("Loopy");
+  } else if (x % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(x);
+  }
+}*/
+
+/*const amounts = [61.00, 52.25, 112.99, 5.00]; 
+let total = 0;
+for (let i = 0; i < amounts.length; i++) {
+  total += amounts[i];
+}
+console.log('Order total is: ', total);*/
+
+
+
