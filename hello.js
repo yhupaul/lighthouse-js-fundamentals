f069bc769e06f1ea9da79338290311673948b60e
yhupaul <yhupaul@hotmail.com>
2021-06-09 18:29:15 -0700

    more examples

--- hello.js
+++ hello.js
@@ -4,8 +4,19 @@
 
 sayHello();*/
 
-const sayHello = function (name) {
+/*const sayHello = function (name) {
   console.log("Hello, " + name);
 }
 sayHello("dan");
-sayHello("mike");
+sayHello("mike");*/
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('john');
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('john');
+console.log(greeting);
